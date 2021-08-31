c1458db6875642c413b252da3f60e3b6d80a6a31
shadowplay1 <43500997+shadowplay1@users.noreply.github.com>
2021-08-31 20:41:00 +0300

    Delete install.js

--- install.js
+++
@@ -1,8 +0,0 @@
-console.log()
-console.log('\x1b[32m――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――')
-console.log('\x1b[36mThank you for installing Discord Leveling Super!')
-console.log('\x1b[32m――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――')
-console.log('\x1b[32mIf you have any questions or need help, join the Support Server:')
-console.log('\x1b[34mhttps://discord.gg/afUTRzfb')
-console.log('\x1b[32m――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――\x1b[37m')
-console.log()
