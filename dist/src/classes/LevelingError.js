ac4a33c3a31b5ef52d448bd7d5ec6c4ff2a56c76
shadowplay1 <43500997+shadowplay1@users.noreply.github.com>
2021-08-31 17:36:04 +0300

    Add files via upload

---
+++ dist/src/classes/LevelingError.js
@@ -0,0 +1,33 @@
+// This file was generated automatically!
+// I'm not responsible for the quality of this code!
+
+// The module is made in TypeScript.
+// See the source code here:
+// https://github.com/shadowplay1/discord-leveling-super
+
+// Thanks!
+
+"use strict";
+/**
+ * LevelingError class.
+ */
+class LevelingError extends Error {
+    /**
+     * Creates an 'LevelingError' instance.
+     * @param {String | Error} message Error message.
+     */
+    constructor(message) {
+        if (message instanceof Error) {
+            super(message.message);
+            Error.captureStackTrace(this, this.constructor);
+        }
+        else
+            super(message);
+        /**
+         * Error name.
+         * @type {String}
+         */
+        this.name = 'LevelingError';
+    }
+}
+module.exports = LevelingError;
---
+++ dist/src/managers/RanksManager.js
@@ -0,0 +1,280 @@
+// This file was generated automatically!
+// I'm not responsible for the quality of this code!
+
+// The module is made in TypeScript.
+// See the source code here:
+// https://github.com/shadowplay1/discord-leveling-super
+
+// Thanks!
+
+const {
+    Message, GuildMember, User,
+    MessageEmbed, MessageAttachment, Guild,
+    MessageOptions, Channel, TextChannel
+} = require('discord.js')
+
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+const discord_js_1 = require("discord.js");
+const LevelingError_1 = __importDefault(require("../classes/LevelingError"));
+const Errors_1 = __importDefault(require("../structures/Errors"));
+const DatabaseManager_1 = __importDefault(require("./DatabaseManager"));
+/**
+ * Ranks manager methods class.
+ */
+class RanksManager {
+    /**
+     * Database Manager.
+     * @type {DatabaseManager}
+     * @private
+     */
+    database;
+    /**
+     * Discord Bot Client.
+     * @type {Client}
+     * @private
+     */
+    client;
+    /**
+     * Ranks manager methods class.
+     * @param {LevelingOptions} options Leveling options object.
+     */
+    constructor(options, client) {
+        this.client = client;
+        this.database = new DatabaseManager_1.default(options);
+    }
+    /**
+    * Fetches the user's rank.
+    * @param {String | GuildMember | User} member Member or it's ID
+    * @param {String | Guild} guild Guild or it's ID
+    * @returns {RankData} User's rank object.
+    */
+    get(member, guild) {
+        const isUser = member instanceof discord_js_1.GuildMember || member instanceof discord_js_1.User;
+        const isGuild = guild instanceof discord_js_1.Guild;
+        if (typeof member !== 'string' && !isUser)
+            throw new LevelingError_1.default(Errors_1.default.invalidTypes.member + typeof member);
+        if (typeof guild !== 'string' && !isGuild)
+            throw new LevelingError_1.default(Errors_1.default.invalidTypes.guild + typeof guild);
+        const user = isUser ? member.id : member.toString();
+        const botGuild = isGuild ? guild.id : guild.toString();
+        const rank = this.database.fetch(`${botGuild}.${user}`);
+        if (!rank)
+            return {
+                userData: null,
+                xp: null,
+                totalXP: null,
+                multiplier: null,
+                level: null,
+                maxXP: null,
+                difference: null,
+            };
+        return rank;
+    }
+    /**
+     * Shows a level leaderboard for specified server.
+     * @param {String | Guild} guild Guild or it's ID
+     * @returns {LeaderboardData[]} Sorted leaderboard array.
+     */
+    leaderboard(guild) {
+        const isGuild = guild instanceof discord_js_1.Guild;
+        if (typeof guild !== 'string' && !isGuild)
+            throw new LevelingError_1.default(Errors_1.default.invalidTypes.guild + typeof guild);
+        const botGuild = isGuild ? guild.id : guild.toString();
+        const serverData = this.database.fetch(`${botGuild}`);
+        let leaderboard = [];
+        if (!serverData)
+            return [];
+        let users = Object.keys(serverData);
+        let ranks = Object.values(serverData);
+        for (let i in users)
+            leaderboard.push({
+                userID: users[i],
+                xp: ranks[i].xp,
+                totalXP: ranks[i].totalXP,
+                level: ranks[i].level,
+                maxXP: ranks[i].maxXP,
+                difference: ranks[i].difference,
+                multiplier: ranks[i].multiplier,
+                user: this.client.users.cache.get(users[i]),
+                userData: ranks[i].userData
+            });
+        return leaderboard.sort((a, b) => b.totalXP - a.totalXP).filter(x => !isNaN(x.totalXP));
+    }
+    /**
+    * Sets the multiplier for specified user.
+    * @param {Number} multiplier The multimplier number to set.
+    * @param {String | GuildMember | User} member Member or it's ID.
+    * @param {String | Guild} guild Guild or it's ID.
+    * @returns {Boolean} If set successfully: true; else: false
+    */
+    setMultiplier(multiplier, member, guild) {
+        const isUser = member instanceof discord_js_1.GuildMember || member instanceof discord_js_1.User;
+        const isGuild = guild instanceof discord_js_1.Guild;
+        if (typeof member !== 'string' && !isUser)
+            throw new LevelingError_1.default(Errors_1.default.invalidTypes.member + typeof member);
+        if (typeof guild !== 'string' && !isGuild)
+            throw new LevelingError_1.default(Errors_1.default.invalidTypes.guild + typeof guild);
+        if (isNaN(multiplier))
+            throw new LevelingError_1.default(Errors_1.default.invalidTypes.multiplier + typeof multiplier);
+        const user = isUser ? member.id : member.toString();
+        const botGuild = isGuild ? guild.id : guild.toString();
+        return this.database.set(`${botGuild}.${user}.multiplier`, multiplier);
+    }
+}
+module.exports = RanksManager;
+
+// ---------------------------------------
+// Typedefs area starts here...
+// ---------------------------------------
+
+/**
+ * @typedef {Object} VersionData
+ * @property {Boolean} updated Is the module updated.
+ * @property {installedVersion} installedVersion Version of module that you have installed.
+ * @property {packageVersion} packageVersion Latest version of the module.
+*/
+
+/**
+ * @typedef {Object} RankData
+ * @property {UserData} userData User's data object.
+ * @property {Number} level User's level
+ * @property {Number} xp User's amount of XP.
+ * @property {Number} totalXP User's total amount of XP.
+ * @property {Number} maxXP How much XP in total the user need to reach the next level.
+ * @property {Number} difference The difference between max XP and current amount of XP. It shows how much XP he need to reach the next level.
+ * @property {Number} multiplier XP Multiplier.
+*/
+
+/**
+ * @typedef {Object} LeaderboardData Leaderboard data object.
+ * @property {String} userID User's ID.
+ * @property {Number} level User's level.
+ * @property {Number} xp User's amount of XP.
+ * @property {Number} totalXP User's amount of total XP.
+ * @property {Number} maxXP User's data object.
+ * @property {User} user User's data object.
+ * @property {Number} difference User's amount of total XP.
+ * @property {Number} multiplier XP Multiplier.
+*/
+
+/**
+ * @typedef {Object} LevelingOptions Default Leveling options.
+ * @property {String} [storagePath='./leveling.json'] Full path to a JSON file. Default: './leveling.json'.
+ * @property {Boolean} [checkStorage=true] Checks the if database file exists and if it has errors. Default: true
+ * @property {Boolean} [ignoreBots=true] If true, every message from bots won't give them XP. Default: true.
+ * @property {Number} [xp=5] Amount of XP that user will receive after sending a message. Default: 5.
+ * @property {Boolean} [status=true] You can enable or disable the leveling system using this option. Default: true.
+ * @property {Number} [maxXP=300] Amount of XP that user will totally need to reach the next level. This value will double for each level. Default: 300.
+ * @property {String[]} [lockedChannels=[]] Array of channel IDs that won't give XP to users. Default: [].
+ * @property {String[]} [ignoredGuilds=[]] Array of guilds on which none of the members will be given XP. Default: [].
+ * @property {Boolean} [multiplier=1] XP multiplier. Default: 1.
+ * @property {FilterFunction} [filter=() => true] Callback function that must return a boolean value, it will add XP only to authors of filtered messages. Default: null.
+ * @property {Number} [updateCountdown=1000] Checks for if storage file exists in specified time (in ms). Default: 1000.
+ * @property {UpdaterOptions} [updater] Update Checker options object.
+ * @property {ErrorHandlerOptions} [errorHandler] Error Handler options object.
+*/
+
+/**
+ * @typedef {Object} UpdaterOptions Updatee options object.
+ * @property {Boolean} [checkUpdates=true] Sends the update state message in console on start. Default: true.
+ * @property {Boolean} [upToDateMessage=true] Sends the message in console on start if module is up to date. Default: true.
+*/
+
+/**
+ * @typedef {Object} ErrorHandlerOptions
+ * @property {Boolean} [handleErrors=true] Handles all errors on startup. Default: true.
+ * @property {Number} [attempts=5] Amount of attempts to load the module. Use 0 for infinity attempts. Default: 5.
+ * @property {Number} [time=3000] Time between every attempt to start the module (in ms). Default: 3000.
+*/
+
+/**
+ * @typedef {Object} CheckerOptions Options object for an 'Leveling.utils.checkOptions' method.
+ * @property {Boolean} [ignoreInvalidTypes=false] Allows the method to ignore the options with invalid types. Default: false.
+ * @property {Boolean} [ignoreUnspecifiedOptions=false] Allows the method to ignore the unspecified  Default: false.
+ * @property {Boolean} [ignoreInvalidOptions=false] Allows the method to ignore the unexisting  Default: false.
+ * @property {Boolean} [showProblems=false] Allows the method to show all the problems in the console. Default: false.
+ * @property {Boolean} [sendLog=false] Allows the method to send the result in the console. Default: false.
+ * @property {Boolean} [sendSuccessLog=false] Allows the method to send the result if no problems were found. Default: false.
+*/
+
+/**
+ * @typedef {Object} UserData User data object.
+ * @property {String} id User's ID.
+ * @property {String} username User's username.
+ * @property {String} tag User's tag.
+ * @property {String} discriminator User's discriminator.
+*/
+
+/**
+ * @typedef {Object} LevelData
+ * @property {Number} xp User's amount of XP.
+ * @property {Number} totalXP User's total amount of XP.
+ * @property {Number} level User's level.
+ * @property {Number} maxXP How much XP in total the user need to reach the next level.
+ * @property {Number} difference The difference between max XP and current amount of XP. It shows how much XP he need to reach the next level.
+ * @property {Number} multiplier User's XP multiplier.
+ * @property {Boolean} onMessage The value will be true if the event was called on 'messageCreate' bot event.
+*/
+
+/**
+ * @typedef {Object} XPData
+ * @property {String} guildID Guild ID.
+ * @property {String} userID User ID.
+ * @property {Number} xp User's amount of XP.
+ * @property {Number} totalXP User's total amount of XP.
+ * @property {Number} level User's level.
+ * @property {Number} maxXP How much XP in total the user need to reach the next level.
+ * @property {Number} difference The difference between max XP and current amount of XP. It shows how much XP he need to reach the next level.
+ * @property {Number} multiplier User's XP multiplier.
+ * @property {Boolean} onMessage The value will be true if the event was called on 'messageCreate' bot event.
+*/
+
+/**
+ * @typedef {Object} LevelUpData
+ * @property {String} guildID Guild ID.
+ * @property {User} user The user that reached a new level.
+ * @property {Number} level New level.
+ * @property {Number} maxXP How much XP in total the user need to reach the next level.
+ * @property {Number} difference The difference between max XP and current amount of XP. It shows how much XP he need to reach the next level.
+ * @property {Number} multiplier User's XP multiplier.
+ * @property {Boolean} onMessage The value will be true if the event was called on 'messageCreate' bot event.
+*/
+
+/**
+ * @typedef {Object} SettingsTypes
+ * @property {Number} xp Amount of XP that user will receive after sending a message.
+ * @property {Number} maxXP Amount of XP that user will totally need to reach the next level. This value will double for each level.
+ * @property {Number} multiplier XP multiplier.
+ * @property {Boolean} status You can enable or disable the leveling system using this option.
+ * @property {String[]} ignoredUsers Array of user IDs that won't give XP.
+ * @property {String[]} lockedChannels Array of channel IDs that won't give XP to users.
+ * @property {Boolean} ignoreBots If true, every message from bots won't give them XP.
+ * @property {String | FilterFunction} filter Callback function that must return a boolean value, it will add XP only to authors of filtered messages.
+ */
+
+/**
+ * @typedef {Object} SettingsArrays
+ * @property {String[]} ignoredUsers Array of user IDs that won't give XP.
+ * @property {String[]} lockedChannels Array of channel IDs that won't give XP to users.
+ */
+
+/**
+ * A function that will send a specified message to a specified channel.
+ * @callback SendMessage
+ * @param {String | MessageEmbed | MessageAttachment | MessageOptions} msg Message string, embed, attachment or message options.
+ * @param {String | Channel} channel Channel or it's ID.
+ * @returns {Promise<Message>}
+ */
+
+/**
+ * Filter function that accepts a message;
+ * it must return a boolean value and it will add XP
+ * only to authors of filtered messages.;
+ * Use 'null' to disable the filter. Default: '() => true'.
+ * @callback FilterFunction
+ * @param {Message} msg
+ * @returns {Boolean} Boolean value.
+*/
