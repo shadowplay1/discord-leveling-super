1ceef1a10170f403005496e39ddabaf1cbe9e2f5
shadowplay1 <43500997+shadowplay1@users.noreply.github.com>
2021-09-22 01:24:51 +0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,20 @@
+node build.js
+
+
+echo "Thanks for using Discord Leveling Super!"
+
+
+echo "---------------------------"
+echo "| Build finished.         |"
+echo "--------------------------|"
+
+echo "| All compiled files are  |"
+echo "| in a 'dist' directory.  |"
+
+echo "--------------------------|"
+echo "| Import the 'dist/index' |"
+echo "| file in your code to    |"
+echo "| start using the module. |"
+echo "--------------------------|"
+echo "| Enjoy! :)               |"
+echo "--------------------------|"
